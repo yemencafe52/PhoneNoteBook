63fd5d0519e9d5c4eee15620fc308095b2d191e7
YemenCafe52 <121191355+yemencafe52@users.noreply.github.com>
2023-03-03 19:51:05 +0300

    up2

---
+++ DBIOLib/ClsDBDS.cs
@@ -0,0 +1,45 @@
+﻿namespace DBIOLib
+{
+    using System.Data;
+
+    /// <summary>
+    /// DataBase IO class
+    /// </summary>
+    public class DBDS : IDBDS
+    {
+        private readonly string connectionString;
+
+        /// <summary>
+        /// DBDS Constrct
+        /// </summary>
+        /// <param name="connectionString"></param>
+        public DBDS(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        /// <summary>
+        /// Execute None Query Stetment;
+        /// </summary>
+        /// <param name="sql">command stetment</param>
+        /// <returns>number of efected rows in database</returns>
+        public int ExecuteNonQuery(string sql)
+        {
+            int res = 0;
+            return res;
+        }
+
+        /// <summary>
+        /// Execte Query 
+        /// </summary>
+        /// <param name="sql">command stetment</param>
+        /// <param name="dataTable">pointer to datatbale to fill it</param>
+        /// <returns>true if success else false</returns>
+        public bool ExecuteQuery(string sql, out DataTable dataTable)
+        {
+            bool res = false;
+            dataTable = null;
+            return res;
+        }
+    }
+}
---
+++ DBIOLib/IDBDS.cs
@@ -0,0 +1,25 @@
+﻿namespace DBIOLib
+{
+    using System.Data;
+
+    /// <summary>
+    /// IO DataBase Interface
+    /// </summary>
+    public interface IDBDS
+    {
+        /// <summary>
+        /// Execute Non Query; Insert,Update,Delete Stetements
+        /// </summary>
+        /// <param name="sql">Command Stetment</param>
+        /// <returns>Rows Number; Effected Rows</returns>
+        int ExecuteNonQuery(string sql);
+
+        /// <summary>
+        /// Execute Select Qury Stetment;
+        /// </summary>
+        /// <param name="sql">Command Stetment</param>
+        /// <param name="dataTable">OutPut Var; Fill This Var With Returned Rows</param>
+        /// <returns></returns>
+        bool ExecuteQuery(string sql, out DataTable dataTable);
+    }
+}
